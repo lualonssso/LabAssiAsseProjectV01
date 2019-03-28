ece8ef440e562cf073d64786fc69da40442034bf
Chadi <chadiayachi@gmail.com>
2019-03-28 09:34:34 +0200

    Add test case changes

---
+++ src/lab2/AppTest.java
@@ -0,0 +1,54 @@
+package lab2;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.*;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        Service s = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals( 0, s.saveStudent("leie0131", "lucia", 933));
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        Service s = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals( 0, s.saveStudent("1", "ana", 221));
+    }
+}
